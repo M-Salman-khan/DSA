9c2aca1488cab77f8ee100b8d49f961b50d9ebe5
M Salman Khan <thesalmankhan2021@gmail.com>
2025-06-22 22:58:50 +0530

    Learn array algorithm methods

---
+++ 02_Array/Two_Pointer/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+// 
+int main(){
+    int arr[7] ={-15, 5, 15, 22, 1,2, 24};
+    int start=0,end=6;
+    cout<<"Original Array : ";
+    for(int i=0; i<7; i++){
+        cout << arr[i] << " ";
+    }
+    cout<<endl;
+    while(start<=end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+    cout<<"Reversed Array : ";
+    for(int i=0; i<7; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
