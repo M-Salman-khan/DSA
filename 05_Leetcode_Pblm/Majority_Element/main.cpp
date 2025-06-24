472c55f19cf28c13c9feb0a18b6cd73d64bf6c62
M Salman Khan <thesalmankhan2021@gmail.com>
2025-06-24 21:33:08 +0530

    Try some leetcode pblms

---
+++ 05_Leetcode_Pblm/Majority_Element/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    vector arr<int> = {1,2,2,1,1};
+    int n = arr.size();
+    for (int val:arr){
+        
+    }
+    return 0;
+}
---
+++ PairSum_pblm/main.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<int>pairSum(vector<int> nums,int target){
+    vector<int> arr;
+    int n=nums.size();
+    int i=0,j=n-1;
+    while(i<j){
+        int pair_Sum=nums[i]+nums[j];
+        if(pair_Sum>target){
+            j--;
+        }
+        else if(pair_Sum<target){
+            i++;
+        }
+        else{
+            arr.push_back(i);
+            arr.push_back(j);
+            return arr;
+        }
+    }
+    return arr;
+}
+int main(){
+    vector <int>nums = {2,7,11,15};
+    int target = 26;
+    vector<int> arr = pairSum(nums,target);
+    cout<<"Index : "<<arr[0]<<" and "<<arr[1]<<endl;
+    // for(int i=0;i<arr.size();i++){
+    //     for(int j=i+1;j<arr.size();j++){
+    //         if(arr[i]+arr[j]==target){
+    //             cout<<"Index : "<<i<<" and "<<j<<endl;
+    //         }
+    //     }
+    // }
+
+    return 0;
+}
