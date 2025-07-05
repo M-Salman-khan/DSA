601b4d1bcd17c1251b0d841e59c4ebbdd2f39c98
M Salman Khan <thesalmankhan2021@gmail.com>
2025-07-05 17:26:41 +0530

    Solve Remove All occurence of substring pblm LC #1910

---
+++ Strings/LC_Questions/1910_Remove_All_Occurrences_of_a_Substring/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+string removeOccurrences(string s, string part) {
+    while(s.length()>=0 && s.find(part)<s.length()){
+        s.erase(s.find(part),part.length());
+    }
+    return s;
+}
+int main(){
+    cout<<removeOccurrences("axxxxyyyyb","xy");
+    return 0;
+}
