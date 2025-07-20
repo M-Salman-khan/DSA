c4c49e9363c814c940408ae579011bd2969aa108
M Salman Khan <thesalmankhan2021@gmail.com>
2025-07-21 02:08:08 +0530

    Time: 0 ms (100%), Space: 64.96 MB (90.12%) - LC #287

---
+++ 06_Hashing/Leetcode_Problems/003__287_Find_the_Duplicate_Number/main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int findDuplicate(vector<int>& arr) {
+    int slow=arr[0],fast=arr[0];
+    do{
+        slow=arr[slow];
+        fast=arr[arr[fast]];
+    }while(slow!=fast);
+    slow=arr[0];
+    while(slow!=fast){
+        slow=arr[slow];
+        fast=arr[fast];
+    };
+    return slow;
+}
+int main(){
+    vector<int>arr={3,1,3,4,2};
+    cout<<findDuplicate(arr);
+    return 0;
+}
