8c5f0e35d552c1901cfc05ca06794c6d25f4ac26
M Salman Khan <thesalmankhan2021@gmail.com>
2025-10-03 10:53:37 +0530

    Learn Insertion of a node at diffrent positions

---
+++ 09_LinkedList/Insertion_of_node/main.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+#include<string>
+using namespace std;
+struct Node{
+    int data;
+    struct Node *next;
+};
+void addNodeAtGivenPOS(Node* &head){
+    struct Node *insertedNode, *temp;
+    insertedNode=(struct Node*)malloc(sizeof(struct Node));
+    int pos,i=1;
+    cout<<"Enter position that you want to insert the node : ";
+    cin>>pos;
+    cout<<"Enter Node data: ";
+    cin>>insertedNode->data;
+
+    if(pos==1){
+        insertedNode->next=head;
+        head=insertedNode;
+    }
+    else{
+        temp=head;
+        while(i<pos){
+            temp=temp->next;
+            i++;
+        }
+    }
+    temp->next=insertedNode;
+}
+void addNodeAtEnd(Node* &head){
+    struct Node *insertedNode, *temp;
+    insertedNode=(struct Node*)malloc(sizeof(struct Node));
+    cout<<"Enter node data that you want to insert at end : ";
+    cin>>insertedNode->data;
+    temp=head;
+    while(temp->next!=0){
+        temp=temp->next;
+    }
+    temp->next=insertedNode;
+}
+void addNodeAtBegining(Node* &head){
+    struct Node *insertedNode;
+    insertedNode=(struct Node*)malloc(sizeof(struct Node));
+    cout<<"Enter node data that you want to insert at beginning : ";
+    cin>>insertedNode->data;
+    insertedNode->next=head;
+    head=insertedNode;
+}
+void printNodes(Node* &temp,Node* &head){
+    temp=head;
+    while(temp!=0){
+        cout<<temp->data<<endl;
+        temp=temp->next;
+    }
+}
+int main(){
+    struct Node *head, *insertedNode,*newNode,*temp;
+    head=0;
+    int choice=1;
+    while(choice){
+        newNode=(struct Node*)malloc(sizeof(struct Node));
+        cout<<"Enter data : ";
+        cin>>newNode->data;
+        newNode->next=0;
+
+        if(head==0){
+            head=newNode;
+            temp=newNode;
+        }
+        else{
+            temp->next=newNode;
+            temp=newNode;
+        }
+        cout<<"Do you want to continue (0,1): ";
+        cin>>choice;
+    }
+    
+    // addNodeAtBegining(head);
+    // printNodes(temp,head);
+    // addNodeAtEnd(head);
+    addNodeAtGivenPOS(head);
+    printNodes(temp,head);
+    return 0;
+}
